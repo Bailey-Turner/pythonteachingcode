0150ac7bcfe022f20f364c0bd22765dd71d82d92
Bailey-Turner <bturn116@students.kennesaw.edu>
2026-02-15 16:47:51 -0500

    update

---
+++ P1 Python Absolute Beginner/P1M1BaileyTurner.py
@@ -0,0 +1,16 @@
+# Create name check code
+
+# [ ] get input for input_test variable
+input_test = input("Who are some people you have met in the last 24 hours: ")
+# [ ] print "True" message if "John" is in the input or False message if not
+print("John" in input_test.title())
+
+# [ ] print True message if your name is in the input or False if not
+my_name = "Bailey"
+print(my_name in input_test.title())
+
+# [ ] Challenge: Check if another person's name is in the input - print message
+print("Joel" in input_test.title())
+
+# [ ] Challenge: Check if a fourth person's name is in the input - print message
+print("Sarah" in input_test.title())
